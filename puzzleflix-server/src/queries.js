d752233860d8f3d13cc7a4d23a5cc26f45e1efcc
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-06-21 17:53:26 +0100

    Remove extraneous env options for DB connection

--- puzzleflix-server/src/queries.js
+++ puzzleflix-server/src/queries.js
@@ -4,61 +4,23 @@ console.log(environment);
 
 // Connect to db
 
-if (environment == "LOCAL") {
-    const pool = mysql.createConnection({
-        user: "cs3099user15",
-        host: "68.183.38.239",
-        database: "cs3099user15_PuzzleFlix", // public database
-        password: "y!pqA34S8sgEJy", // key to our school server's mariaDB 
-        port: 3306,
-    });
+const pool = mysql.createConnection({
+    connectionLimit: 10,
+    user: "cs3099user15",
+    host: "68.183.38.239",
+    database: "cs3099user15_PuzzleFlix", // public database
+    password: "y!pqA34S8sgEJy", // key to our school server's mariaDB 
+    port: 3306,
+    waitForConnections: true,
+    timeout: 60000 // 60 seconds
+});
 
-    pool.connect((err) => {
-        if (err) {
-            console.error(err);
-            throw err;
-        } else {
-            console.log("Database Connected!");
-        }
-    });
-    module.exports = pool;
-} else if (environment == "PRODUCTION") {
-    const pool = mysql.createConnection({
-        user: "cs3099user15",
-        host: "68.183.38.239",
-        database: "cs3099user15_PuzzleFlix", // public database
-        password: "y!pqA34S8sgEJy", // key to our school server's mariaDB 
-        port: 3306,
-    });
-
-    pool.connect((err) => {
-        if (err) {
-            console.log("PROD FAIL!");
-            throw err;
-        } else {
-            console.log("Database Connected!");
-        }
-    });
-
-    module.exports = pool;
-} else if (environment == "TEST") {
-    const pool = mysql.createConnection({
-        user: "cs3099user15",
-        host: "cs3099user15.host.cs.st-andrews.ac.uk",
-        database: "cs3099user15_PuzzleFlix_Test", // Test database
-        password: "y!pqA34S8sgEJy", // key to our school server's mariaDB 
-        port: 3306,
-    });
-    console.log('test database connected');
-    pool.connect((err) => {
-        if (err) {
-            //console.log("TESTA FAIL!");
-            throw err;
-        } else {
-            //console.log("Database Connected!");
-        }
-    });
-    module.exports = pool;
-} else {
-    console.log("Incorrect environemnt: Options are LOCAL and PRODUCTION");
-}
+pool.connect((err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log("Database Connected!");
+    }
+});
+module.exports = pool;
