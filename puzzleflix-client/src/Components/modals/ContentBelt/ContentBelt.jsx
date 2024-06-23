03baf8daccd6c996aff871c76a46e9f0ae7f8e7d
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-06-23 15:30:43 +0100

    Rm fedpuzzles

--- puzzleflix-client/src/Components/modals/ContentBelt/ContentBelt.jsx
+++ puzzleflix-client/src/Components/modals/ContentBelt/ContentBelt.jsx
@@ -1,8 +1,7 @@
 // Libraries
-import React, { useEffect, useLayoutEffect, useRef } from "react";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Axios from "axios";
-import { port, fullurl } from "./../../../Config";
+import { fullurl } from "./../../../Config";
 
 // Styling
 import "./ContentBelt.css";
@@ -16,91 +15,46 @@ import { ReactComponent as RightArrow } from "./../../../assets/right-arrow.svg"
 import { ReactComponent as LeftArrow } from "./../../../assets/left-arrow.svg";
 
 function ContentBelt(props) {
-    const { name, getPuzzle, type, theme } = props;
-    const {user} = type=="userspuzzles" ? props : ""
+    const { name, type, theme } = props;
 
     const formattedName = name.split(" ").join("");
 
     const [puzzles, setPuzzles] = useState([]);
-
     const [sortValue, setSortValue] = useState(0);
-
     const pageIndex = useRef(0);
-
     const [total, setTotal] = useState(0);
 
     // Gets the next page of 10 puzzles (called when requesting next page in content-belt)
     const getNext = () => {
-        const thumbnails = document.querySelectorAll(
-            `#${formattedName} > .thumbnail-link`
-        );
-        for (let thumbnail of thumbnails) {
-            thumbnail.classList.add("next");
-        }
-        setTimeout(() => {
-            for (let thumbnail of thumbnails) {
-                thumbnail.classList.remove("next");
-            }
-            console.log("GET NEXT");
-            pageIndex.current++;
-
-            if (type == "fedpuzzles") {
-                getFedPuzzles(sortValue);
-            } else {
-                sort(sortValue);
-            }
-        }, 200);
+        animateThumbnails('next');
+        pageIndex.current++;
+        sort(sortValue);
     };
 
     // Gets the previous page of 10 puzzles
     const getPrev = () => {
-        const thumbnails = document.querySelectorAll(
-            `#${formattedName} > .thumbnail-link`
-        );
-        for (let thumbnail of thumbnails) {
-            thumbnail.classList.add("prev");
-        }
+        animateThumbnails('prev');
+        pageIndex.current--;
+        sort(sortValue);
+    };
 
-        setTimeout(() => {
-            for (let thumbnail of thumbnails) {
-                thumbnail.classList.remove("prev");
-            }
-            console.log("GET PREV");
-            pageIndex.current--;
-
-            if (type == "fedpuzzles") {
-                getFedPuzzles(sortValue);
-            } else {
-                sort(sortValue);
-            }
-        }, 200);
+    // Animate thumbnails for visual effect
+    const animateThumbnails = (direction) => {
+        const thumbnails = document.querySelectorAll(`#${formattedName} > .thumbnail-link`);
+        thumbnails.forEach(thumbnail => thumbnail.classList.add(direction));
+        setTimeout(() => thumbnails.forEach(thumbnail => thumbnail.classList.remove(direction)), 200);
     };
 
     // Gets the first 10 puzzles with particular sort
     const sort = async (sortVal) => {
-        let puzzletype = type == "eights" ? "eights" : "sudoku";
-
-        console.log("SORT");
-        let mode = "orderByDifficultyAsc";
-        if (sortVal == 1) {
-            mode = "sortDateDesc";
-        } else if (sortVal == 2) {
-            mode = "sortDateAsc";
-        } else if (sortVal == 2) {
-            mode = "orderByDifficultyAsc";
-        } else if (sortVal == 3) {
-            mode = "orderByDifficultyDesc";
-        } else if (sortVal == 4) {
-            mode = "orderByNameAsc";
-        } else if (sortVal == 5) {
-            mode = "orderByNameDesc";
-        }
+        let mode = determineSortMode(sortVal);
+        let puzzletype = type === "eights" ? "eights" : "sudoku";
 
         try {
             const res = await Axios({
                 method: "POST",
                 withCredentials: true,
-                url: fullurl + "/paginatedpuzzle",
+                url: `${fullurl}/paginatedpuzzle`,
                 data: {
                     userid: localStorage.getItem("userid"),
                     accessToken: localStorage.getItem("accessToken"),
@@ -109,7 +63,6 @@ function ContentBelt(props) {
                     index: pageIndex.current * 10,
                     mode: mode,
                     puzzletype: puzzletype,
-                    user: user,
                 },
             });
             let data = res.data;
@@ -120,58 +73,25 @@ function ContentBelt(props) {
         }
     };
 
-    /**
-     * Retrieves paginated federation puzzles
-     * @param {*} sortVal : the ordering type
-     */
-    const getFedPuzzles = async (sortVal) => {
-        console.log("FEDPUZZLES");
-
-        let mode = "orderByDifficultyAsc";
-        if (sortVal == 1) {
-            mode = "orderByDifficultyAsc";
-        } else if (sortVal == 2) {
-            mode = "orderByDifficultyDesc";
-        } else if (sortVal == 3) {
-            mode = "orderByNameAsc";
-        } else if (sortVal == 4) {
-            mode = "orderByNameDesc";
-        }
-
-        try {
-            const res = await Axios({
-                method: "GET",
-                withCredentials: false,
-                url: fullurl + "/fedapi/fedpuzzles",
-                params: {
-                    type: type,
-                    count: 10,
-                    index: pageIndex.current * 10,
-                    mode: mode,
-                },
-            });
-
-            let data = res.data;
-            await setPuzzles(data[0]);
-            await setTotal(data[1]);
-        } catch (err) {
-            // console.error(err);
-            console.log("Failed to retrieve fed puzzles");
+    // Determine sort mode based on value
+    const determineSortMode = (sortVal) => {
+        switch (sortVal) {
+            case 1: return "sortDateDesc";
+            case 2: return "sortDateAsc";
+            case 3: return "orderByDifficultyAsc";
+            case 4: return "orderByDifficultyDesc";
+            case 5: return "orderByNameAsc";
+            case 6: return "orderByNameDesc";
+            default: return "orderByDifficultyAsc";
         }
     };
 
-    /**
-     * useEffect for loading the puzzles of the content belt
-     */
+    // useEffect for loading the puzzles of the content belt
     useEffect(() => {
-        if (type == "fedpuzzles") {
-            getFedPuzzles(1);
-        } else {
-            sort(1);
-        }
+        sort(1);  // Default sorting when the component mounts
     }, []);
 
-    if (puzzles.filter((e) => e.puzzleid != "queens").length > 0) {
+    if (puzzles.length > 0) {
         return (
             <div className="scroll" id={type.toLowerCase()}>
                 <div className="contentbelt-header">
@@ -180,59 +100,33 @@ function ContentBelt(props) {
                         theme={theme}
                         sortValue={sortValue}
                         setSortValue={setSortValue}
-                        sort={type == "fedpuzzles" ? getFedPuzzles : sort}
+                        sort={sort}
                         pageIndex={pageIndex}
                     />
                 </div>
                 <div className="content-belt" id={formattedName}>
-                    {pageIndex.current >= 1 ? (
-                        <>
-                            <button
-                                aria-label="Get next 10 puzzles"
-                                className="arrow-btn"
-                                onClick={() => getPrev()}
-                            >
-                                <LeftArrow className="arrow-btn-symbol" />
-                            </button>
-                        </>
-                    ) : (
-                        ""
+                    {pageIndex.current > 0 && (
+                        <button aria-label="Get previous 10 puzzles" onClick={getPrev} className="arrow-btn">
+                            <LeftArrow className="arrow-btn-symbol" />
+                        </button>
                     )}
-
-                    {puzzles.map((puzzle, index) => {
-                        return (
-                            <PuzzleThumbnail
-                                type={type}
-                                key={index}
-                                puzzle={puzzle}
-                                puzzledata={
-                                    type == "fedpuzzles"
-                                        ? puzzle.puzzle
-                                        : puzzle.puzzledata
-                                }
-                                name={puzzle.puzzlename}
-                                puzzleid={puzzle.puzzleid}
-                            />
-                        );
-                    })}
-
-                    {pageIndex.current < Math.floor(total / 10) ? (
-                        <>
-                            <button
-                                aria-label="Get next 10 puzzles"
-                                onClick={() => getNext()}
-                                className="arrow-btn"
-                            >
-                                <RightArrow className="arrow-btn-symbol" />
-                            </button>
-                        </>
-                    ) : (
-                        ""
+                    {puzzles.map((puzzle, index) => (
+                        <PuzzleThumbnail
+                            type={type}
+                            key={index}
+                            puzzle={puzzle}
+                            puzzledata={puzzle.puzzledata}
+                            name={puzzle.puzzlename}
+                            puzzleid={puzzle.puzzleid}
+                        />
+                    ))}
+                    {pageIndex.current < Math.floor(total / 10) && (
+                        <button aria-label="Get next 10 puzzles" onClick={getNext} className="arrow-btn">
+                            <RightArrow className="arrow-btn-symbol" />
+                        </button>
                     )}
                 </div>
             </div>
         );
     }
-}
-
-export default ContentBelt;
+    return null; // Return null if no puzzles
