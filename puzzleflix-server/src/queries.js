5d29de9c61a7898d4cb1bbf15f2fd45caff98894
Vanaj Moorthy <vanajmoorthy@gmail.com>
2024-06-28 10:08:31 +0100

    Handle disconnect

--- puzzleflix-server/src/queries.js
+++ puzzleflix-server/src/queries.js
@@ -15,12 +15,39 @@ const pool = mysql.createConnection({
     timeout: 60000 // 60 seconds
 });
 
-pool.connect((err) => {
-    if (err) {
-        console.error(err);
-        throw err;
+function handleDisconnect(conn) {
+    conn.on('error', function (error) {
+        if (error.code === 'PROTOCOL_CONNECTION_LOST' || error.code === 'ECONNRESET') {
+            console.error('Database connection was closed.');
+            reconnect(conn);
+        } else {
+            throw error;
+        }
+    });
+}
+
+function reconnect(conn) {
+    console.log('Reconnecting to database...');
+    // Create a new connection
+    conn = mysql.createConnection(conn.config);
+    handleDisconnect(conn);
+    conn.connect(function (error) {
+        if (error) {
+            console.error('Error when reconnecting to the database:', error);
+            setTimeout(reconnect, 2000, conn);
+        } else {
+            console.log('Successfully reconnected to the database.');
+        }
+    });
+}
+
+handleDisconnect(pool);
+
+pool.connect(error => {
+    if (error) {
+        console.error('Error connecting to the database:', error);
     } else {
-        console.log("Database Connected!");
+        console.log('Connected to the database.');
     }
 });
 module.exports = pool;
